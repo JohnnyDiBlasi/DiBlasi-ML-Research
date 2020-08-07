10489971242dff5ff40900d19cf50b06b9fcd090
Johnny DiBlasi <49796738+JohnnyDiBlasi@users.noreply.github.com>
2020-08-07 15:54:28 -0500

    Update jdml-matrix.java

--- jdml-matrix.java
+++ jdml-matrix.java
@@ -3,9 +3,17 @@ class Matrix {
   int cols;
   float[][] data;
   
+  Matrix() {
+  }
+  
   Matrix(int theRows, int theCols) {
-  rows = theRows;
-  cols = theCols;
-  data = new float[rows][cols];
+    rows = theRows;
+    cols = theCols;
+    data = new float[rows][cols];
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        data[i][j] = 0;
+      }
+    }
   }
 }
